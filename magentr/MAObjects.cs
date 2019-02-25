10977364deee7e94aa519e19b72b34e583e3dc93
Mo Chen <chenmosu@cn.ibm.com>
2019-02-25 21:30:31 +0800

    Version 0.0.0.1 First Commit.

---
+++ magentr/MAObjects.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using EXCEL = Microsoft.Office.Interop.Excel;
+
+namespace magentr
+{
+    class MAObjects
+    {
+    }
+
+    class RequestSheet
+    {
+        public RequestColumns Col1, Col2, Col3;
+        //Instantiate an empty sheet object, which contains 3 column objects.
+        public RequestSheet(
+            RequestColumns col1
+            , RequestColumns col2
+            , RequestColumns col3)
+        {
+            Col1 = col1;
+            Col2 = col2;
+            Col3 = col3;
+        }
+
+    }
+
+    class RequestColumns
+    {
+        //Instantiate an empty Column object, which contains 3 column objects.
+    }
+
+    class MAgents
+    {
+        //Instantiate an empty Agent object, which contains M/Agent Information.
+
+    }
+
+    class Servers
+    {
+        public string Hostname;
+        public bool IsCluster = false;
+        public enum NetworkLocation
+        {
+            JDC,GDC,DRC,Others
+        }
+        public enum NetworkArea
+        {
+            SEN,SecureSEN,SecureGlobal,Global
+            ,SecureLocal,DMZ,iDC,SAZ
+        }
+        public enum Cluster
+        {
+            VIP,PRI,SEC
+        }
+        public Servers VIP;
+        public Servers PRI;
+        public Servers SEC;
+        public IPAddress IP_Address;
+        public string Maker;
+        public string Model;
+        public int CPU_Number;
+        public int CPU_Microprocessor;
+        public string OS;
+        public string OSVersion;
+        public int OSBit;
+        public int BoxSplit;
+        public int BoxThis;
+        //Instantiate an empty Server object, which server information.
+
+        public Servers()
+        {
+            //return a string that represents the object.
+        }
+        public Servers(string hostname)
+        {
+            this.Hostname = hostname;
+        }
+        public Servers(string hostname, IPAddress ip_Address)
+        {
+            this.Hostname = hostname;
+            IP_Address = ip_Address;
+        }
+        public Servers(string starting_cell, EXCEL.Worksheet xlsheet)
+        {
+            //Fetching a Server info based on Server Area Directly.
+            //Examples for starting cell $H$51
+            EXCEL.Range StartingCell = xlsheet.Range["$H$51"];
+
+            
+            this.Hostname = StartingCell.Value as string;
+            this.IP_Address = IPAddress
+                .Parse(StartingCell.Offset[1, 1].Value);
+            this.Maker = StartingCell.Offset[2, 0].Value as string;
+            this.Model = StartingCell.Offset[3, 0].Value as string;
+            this.CPU_Number = (int)StartingCell.Offset[4, 0].Value;
+            this.CPU_Microprocessor = (int)StartingCell.Offset[5, 0].Value;
+
+
+            
+        }
+
+    }
+}
--- magentr/MainWindow.xaml.cs
+++ magentr/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Linq;
+using System.Windows.Media;
 using EXCEL = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
 
 namespace magentr
 {
@@ -24,6 +28,7 @@ namespace magentr
         {
             //Initiate Load New Request Form Procedure
             // Step 001 Open file
+            DateTime timeStart = DateTime.Now;
             OpenFileDialog OpenFileNew = new OpenFileDialog();
             OpenFileNew.DefaultExt = ".xlsx;.xls";
             OpenFileNew.Filter = "Excel Worksheet (.xls;.xlsx)|*.xls;*.xlsx";
@@ -38,10 +43,14 @@ namespace magentr
             {
                 MessageBox.Show("No file selected.");
             }
+            Debug.Print(string.Format("Button Click Ran for: {0}", 
+                (DateTime.Now - timeStart).ToString("hh':'mm':'ss")));
         }
 
         private async void FetchNewRequest(string dirNew)
         {
+            DateTime timeStart = DateTime.Now;
+
             var UpdateProgressBar = new Progress<int>(value => pbarMain.Value = value);
             var SetProgressBarMax = new Progress<int>(value => pbarMain.Maximum = value);
 
@@ -57,6 +66,7 @@ namespace magentr
             IProgress<int> reportProgress, 
             IProgress<int> setProgressMax)
         {
+            DateTime timeStart = DateTime.Now;
             setProgressMax.Report(100);
             reportProgress.Report(1);
             if (!inputfile.Exists) throw new Exception("M/Agent Application File Does not Exist!");
@@ -64,32 +74,62 @@ namespace magentr
             EXCEL.Workbooks xlWorkbooks = xlApp.Workbooks; reportProgress.Report(40);
             EXCEL.Workbook xlWbk = xlWorkbooks.Open(inputfile.FullName); reportProgress.Report(60);
             EXCEL.Worksheet xlSht = xlWbk.ActiveSheet; reportProgress.Report(80);
-            EXCEL.Range xlRange = xlSht.UsedRange;
+            // EXCEL.Range xlRange = xlSht.UsedRange;
             reportProgress.Report(100);
-            MessageBox.Show("Loading Completed.");
+            Debug.Print("Loading Completed.");
+
             #region ---- Fetch M/Agent Information ----
             //Calculating Total Work Load:
-            int WorkLoad_Total = xlRange.Count;
-            setProgressMax.Report(WorkLoad_Total);
-            int WorkdLoad_Current = 0;
+            //int WorkLoad_Total = xlRange.Count;
+            //setProgressMax.Report(WorkLoad_Total);
+            //int WorkdLoad_Current = 0;
             //Loading 
-            foreach(EXCEL.Range r in xlRange)
-            {
-                reportProgress.Report(++WorkdLoad_Current);
-            }
-            reportProgress.Report(0);
+            //foreach(EXCEL.Range r in xlRange)
+            //{
+            //    reportProgress.Report(++WorkdLoad_Current);
+            //}
+            //reportProgress.Report(0);
+
             EXCEL.Shapes xlShapes = xlSht.Shapes;
 
-            WorkLoad_Total = xlShapes.Count;
+            int WorkLoad_Total = xlShapes.Count;
             setProgressMax.Report(WorkLoad_Total);
-            WorkdLoad_Current = 0;
-            foreach(EXCEL.Shape s in xlShapes)
+            int WorkdLoad_Current = 0;
+
+            IEnumerable<EXCEL.Shape> xlCheckBoxes =
+                from EXCEL.Shape s in xlShapes
+                where s.Name.Contains("チェック") 
+                select s;
+
+            Dictionary<string, string> dicCheckedBoxes = 
+                new Dictionary<string, string>();
+            setProgressMax.Report(xlCheckBoxes.Count());
+            foreach(EXCEL.Shape s in xlCheckBoxes)
             {
+                s.TopLeftCell.Interior.Color = EXCEL.XlRgbColor.rgbRed;
+                Debug.Print("|{0,-14}|{1,-20}|{2,-20}|"
+                    , s.Name
+                    , (string)s.TopLeftCell.Offset[0, 1].Value
+                    , ((double)s.OLEFormat.Object.Value).ToString());
+                dicCheckedBoxes.Add(
+                    s.TopLeftCell.Address
+                    ,(string)s
+                    .TopLeftCell
+                    .Offset[0, 1]
+                    .Value);
                 reportProgress.Report(++WorkdLoad_Current);
             }
 
+            setProgressMax.Report(0);
+            CheckBoxValue(
+                xlSht.Range["H32","K33"]
+                , dicCheckedBoxes
+                , reportProgress
+                , setProgressMax
+                , out string TestCheckBoxValue);
+            Debug.Print(TestCheckBoxValue);
 
-
+            //MessageBox.Show(TestCheckBoxValue);
 
             #endregion ---- Fetch M/Agent Information ----
             xlWbk.Close();
@@ -99,7 +139,47 @@ namespace magentr
             Marshal.ReleaseComObject(xlWorkbooks);
             Marshal.ReleaseComObject(xlWbk);
             Marshal.ReleaseComObject(xlSht);
-            Marshal.ReleaseComObject(xlRange);
-        } 
+            //Marshal.ReleaseComObject(xlRange);
+            Debug.Print(string.Format("Open Excel Async Ran for: {0}",
+                (DateTime.Now - timeStart).ToString("hh':'mm':'ss")));
+
+
+        }
+
+        private void CheckBoxValue (
+            EXCEL.Range RangeGroup
+            , Dictionary<string, string> dicCheckedBoxes
+            , IProgress<int> reportProgress
+            , IProgress<int> setProgressMax
+            , out string Check_Box_Value)
+        {
+            //Example, Range("H32:K33")
+            string result = null;
+
+            //Dictionary<EXCEL.Range, string> CheckingRange = new Dictionary<EXCEL.Range, string>();
+            reportProgress.Report(0);
+            setProgressMax.Report(RangeGroup.Count);
+            Debug.Print("CheckBoxValue:Started");
+            Debug.Print("Input Range Counter: " + RangeGroup.Count);
+            int Current_Progress = 0;
+            foreach (EXCEL.Range r in RangeGroup)
+            {
+                reportProgress.Report(++Current_Progress);
+                Debug.Print("Checking Address:" + r.Address);
+                if (dicCheckedBoxes.ContainsKey(r.Address))
+                {
+
+                    result = dicCheckedBoxes[r.Address];
+                }
+                //foreach (var k in dicCheckedBoxes.ToList())
+                //{
+                //    Debug.Print((string)r.Value);
+                //    Debug.Print(string.Format("{0}:{1}", k.Key, k.Value));
+                //}
+            }
+
+            Debug.Print("CheckBoxValue:Ended");
+            Check_Box_Value = result;
+        }
     }
 }
