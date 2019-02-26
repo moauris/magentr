eaf3e0a779dd3c63da73e786442fe50f0c064349
Mo Chen <chenmosu@cn.ibm.com>
2019-02-26 18:53:22 +0800

    v0.0.0.1 Second Update.

--- magentr/MainWindow.xaml.cs
+++ magentr/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Windows.Media;
 using EXCEL = Microsoft.Office.Interop.Excel;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace magentr
 {
@@ -18,7 +19,11 @@ namespace magentr
     public partial class MainWindow : Window
     {
         public string dirNewRequest = "";
-        
+        private Dictionary<string, string> dictRequestRawData
+            = new Dictionary<string, string>();
+        private Dictionary<string, string> dictCheckBox
+            = new Dictionary<string, string>();
+        //private delegate void ConvertRange(EXCEL.Range TargetRange);
         public MainWindow()
         {
             InitializeComponent();
@@ -33,7 +38,7 @@ namespace magentr
             OpenFileNew.DefaultExt = ".xlsx;.xls";
             OpenFileNew.Filter = "Excel Worksheet (.xls;.xlsx)|*.xls;*.xlsx";
             OpenFileNew.ShowDialog();
-            //MessageBox.Show(OpenFileNew + " Selected.");
+            lbxDebug.Items.Add(OpenFileNew + " Selected.");
             dirNewRequest = OpenFileNew.FileName;
             if (dirNewRequest != "")
             {
@@ -41,10 +46,10 @@ namespace magentr
             }
             else
             {
-                MessageBox.Show("No file selected.");
+                lbxDebug.Items.Add("No file selected.");
             }
-            Debug.Print(string.Format("Button Click Ran for: {0}", 
-                (DateTime.Now - timeStart).ToString("hh':'mm':'ss")));
+            lbxDebug.Items.Add((string.Format("Button Click Ran for: {0}", 
+                (DateTime.Now - timeStart).ToString("hh':'mm':'ss"))));
         }
 
         private async void FetchNewRequest(string dirNew)
@@ -53,86 +58,126 @@ namespace magentr
 
             var UpdateProgressBar = new Progress<int>(value => pbarMain.Value = value);
             var SetProgressBarMax = new Progress<int>(value => pbarMain.Maximum = value);
-
+            var PrintDebugListBox = new Progress<string>(value => 
+            {
+                lbxDebug.Items.Add(DateTime.Now.ToString("hh':'mm':'ss") + " % " + value);
+                svDebug.ScrollToBottom();
+            });
             //This procedure fetches information from Excel
             FileInfo InputFile = new FileInfo(dirNew);
             await Task.Run(() =>
             {
-                SyncVonExcel(InputFile, UpdateProgressBar, SetProgressBarMax);
+                SyncVonExcel(InputFile
+                    , UpdateProgressBar
+                    , SetProgressBarMax
+                    , PrintDebugListBox);
             });
         }
 
-        private void SyncVonExcel(FileInfo inputfile, 
-            IProgress<int> reportProgress, 
-            IProgress<int> setProgressMax)
+        private void SyncVonExcel(FileInfo inputfile 
+            , IProgress<int> reportProgressBar
+            , IProgress<int> setProgressBarMax
+            , IProgress<string> printDebugListBox)
         {
+            dictRequestRawData
+            = new Dictionary<string, string>();
+            dictCheckBox
+            = new Dictionary<string, string>();
             DateTime timeStart = DateTime.Now;
-            setProgressMax.Report(100);
-            reportProgress.Report(1);
-            if (!inputfile.Exists) throw new Exception("M/Agent Application File Does not Exist!");
-            EXCEL.Application xlApp = new EXCEL.Application(); reportProgress.Report(20);
-            EXCEL.Workbooks xlWorkbooks = xlApp.Workbooks; reportProgress.Report(40);
-            EXCEL.Workbook xlWbk = xlWorkbooks.Open(inputfile.FullName); reportProgress.Report(60);
-            EXCEL.Worksheet xlSht = xlWbk.ActiveSheet; reportProgress.Report(80);
+            setProgressBarMax.Report(100);
+            reportProgressBar.Report(1);
+            if (!inputfile.Exists) //throw new Exception("M/Agent Application File Does not Exist!");
+            {
+                printDebugListBox.Report("M/Agent Application File Does not Exist!");
+                return;
+            }
+            EXCEL.Application xlApp = new EXCEL.Application(); reportProgressBar.Report(20);
+            EXCEL.Workbooks xlWorkbooks = xlApp.Workbooks; reportProgressBar.Report(40);
+            EXCEL.Workbook xlWbk = xlWorkbooks.Open(inputfile.FullName); reportProgressBar.Report(60);
+            EXCEL.Worksheet xlSht = xlWbk.ActiveSheet; reportProgressBar.Report(80);
             // EXCEL.Range xlRange = xlSht.UsedRange;
-            reportProgress.Report(100);
+            reportProgressBar.Report(100);
             Debug.Print("Loading Completed.");
 
             #region ---- Fetch M/Agent Information ----
-            //Calculating Total Work Load:
-            //int WorkLoad_Total = xlRange.Count;
-            //setProgressMax.Report(WorkLoad_Total);
-            //int WorkdLoad_Current = 0;
-            //Loading 
-            //foreach(EXCEL.Range r in xlRange)
-            //{
-            //    reportProgress.Report(++WorkdLoad_Current);
-            //}
-            //reportProgress.Report(0);
+            printDebugListBox.Report("Beginning Fetching M/Agent Information.");
+            printDebugListBox.Report("Getting Range Dictionary Delegate.");
+
+            void RangeToDict(EXCEL.Range TargetRange)
+            {
+                dictRequestRawData[TargetRange.Address]
+                = (string)TargetRange.Value;
+            }
+
+            //Cell Range: D5, S163
+            EXCEL.Range FormArea = xlSht.Range["D5", "S163"];
+            printDebugListBox.Report("Calculating Total Form Area Ranges");
+            int FormAreaRangCount = FormArea.Count;
+            int FormAreaCurrentCount = 0;
+            printDebugListBox.Report("Total Form Area Ranges : " + FormAreaRangCount);
+            setProgressBarMax.Report(FormAreaRangCount);
+            reportProgressBar.Report(FormAreaCurrentCount);
+
+            foreach (EXCEL.Range r in FormArea)
+            {
+                if(r.Value == null)
+                {
+                    RangeToDict(r);
+                }
+                reportProgressBar.Report(++FormAreaCurrentCount);
+            }
+            printDebugListBox.Report("Sync Target Area Complete.");
+            //Trying to fetch form public Dictionary Object.
 
             EXCEL.Shapes xlShapes = xlSht.Shapes;
 
             int WorkLoad_Total = xlShapes.Count;
-            setProgressMax.Report(WorkLoad_Total);
+            setProgressBarMax.Report(WorkLoad_Total);
             int WorkdLoad_Current = 0;
 
             IEnumerable<EXCEL.Shape> xlCheckBoxes =
                 from EXCEL.Shape s in xlShapes
-                where s.Name.Contains("チェック") 
+                where s.Name.Contains("チェック") //v0.0.0.1 Need a regex to match both en and jp version.
                 select s;
 
-            Dictionary<string, string> dicCheckedBoxes = 
-                new Dictionary<string, string>();
-            setProgressMax.Report(xlCheckBoxes.Count());
+            //Dictionary<string, string> dicCheckedBoxes = 
+            //    new Dictionary<string, string>(); //combine with the dictRequestRawData object
+
+            setProgressBarMax.Report(xlCheckBoxes.Count());
+
             foreach(EXCEL.Shape s in xlCheckBoxes)
             {
-                s.TopLeftCell.Interior.Color = EXCEL.XlRgbColor.rgbRed;
-                Debug.Print("|{0,-14}|{1,-20}|{2,-20}|"
-                    , s.Name
-                    , (string)s.TopLeftCell.Offset[0, 1].Value
-                    , ((double)s.OLEFormat.Object.Value).ToString());
-                dicCheckedBoxes.Add(
-                    s.TopLeftCell.Address
-                    ,(string)s
-                    .TopLeftCell
-                    .Offset[0, 1]
-                    .Value);
-                reportProgress.Report(++WorkdLoad_Current);
+                //s.TopLeftCell.Interior.Color = EXCEL.XlRgbColor.rgbRed;
+                //printDebugListBox.Report(string.Format("|{0,-30}|{1,-30}|{2,-30}|"
+                //    , s.Name
+                //    , (string)s.TopLeftCell.Offset[0, 1].Value
+                //    , ((double)s.OLEFormat.Object.Value).ToString()));
+                if ((double)s.OLEFormat.Object.Value == 1)
+                {
+                    dictCheckBox.Add(
+                        s.TopLeftCell.Address
+                        , (string)s
+                        .TopLeftCell
+                        .Offset[0, 1]
+                        .Value);
+                }
+                reportProgressBar.Report(++WorkdLoad_Current);
             }
-
-            setProgressMax.Report(0);
+            /* Testing if CheckBox Function Works
+            setProgressBarMax.Report(0);
             CheckBoxValue(
                 xlSht.Range["H32","K33"]
                 , dicCheckedBoxes
-                , reportProgress
-                , setProgressMax
+                , reportProgressBar
+                , setProgressBarMax
                 , out string TestCheckBoxValue);
-            Debug.Print(TestCheckBoxValue);
+            Debug.Print(TestCheckBoxValue);*/
 
             //MessageBox.Show(TestCheckBoxValue);
-
+            //Show Entire Value Dictionary Object:
+            
             #endregion ---- Fetch M/Agent Information ----
-            xlWbk.Close();
+            xlWbk.Close(false, Missing.Value, Missing.Value); //This Argument will cause excel to exist without saving.
             xlWorkbooks.Close();
             xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
@@ -140,10 +185,23 @@ namespace magentr
             Marshal.ReleaseComObject(xlWbk);
             Marshal.ReleaseComObject(xlSht);
             //Marshal.ReleaseComObject(xlRange);
-            Debug.Print(string.Format("Open Excel Async Ran for: {0}",
+            setProgressBarMax.Report(dictRequestRawData.Count);
+            int CurrentProgress = 0;
+            foreach (KeyValuePair<string, string> k in dictRequestRawData)
+            {
+                printDebugListBox.Report(string.Format("{0,-7}|{1}", k.Key, k.Value));
+                reportProgressBar.Report(++CurrentProgress);
+            }
+            setProgressBarMax.Report(dictCheckBox.Count);
+            CurrentProgress = 0;
+            foreach (KeyValuePair<string, string> k in dictCheckBox)
+            {
+                printDebugListBox.Report(string.Format("{0,-7}|{1}", k.Key, k.Value));
+                reportProgressBar.Report(++CurrentProgress);
+            }
+            reportProgressBar.Report(0);
+            printDebugListBox.Report(string.Format("Open Excel Async Ran for: {0}",
                 (DateTime.Now - timeStart).ToString("hh':'mm':'ss")));
-
-
         }
 
         private void CheckBoxValue (
@@ -171,11 +229,6 @@ namespace magentr
 
                     result = dicCheckedBoxes[r.Address];
                 }
-                //foreach (var k in dicCheckedBoxes.ToList())
-                //{
-                //    Debug.Print((string)r.Value);
-                //    Debug.Print(string.Format("{0}:{1}", k.Key, k.Value));
-                //}
             }
 
             Debug.Print("CheckBoxValue:Ended");
